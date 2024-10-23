b8aa939fb881f7bfc74f466d302ea154e6aae2e4
AmandaAnguiano <amanda.anguiano@ucas-edu.net>
2024-10-23 19:44:28 +0000

    did a bunch of code
    Co-authored-by: s0fia090 <s0fia090@users.noreply.github.com>
    Co-authored-by: elliefonua <elliefonua@users.noreply.github.com>

---
+++ build.a.game/game.py
@@ -0,0 +1,60 @@
+
+#function of  - amanda
+def body(input):
+    question = (input(f"{input} was added....\n"))
+    
+    return question
+    
+    
+# Ask for letters - ellie
+
+
+word1 = eye
+word2 = skibidi
+word3 = mog
+word4word5 = rizz
+
+l1 = (body("r"))
+l2 = (body("i"))
+l3 = (body("z"))
+l4 = (body("z"))
+
+e1 = (body("e"))
+e1 = (body("y"))
+e3 = (body("e"))
+
+t1 = (body("s"))
+t2 = (body("k"))
+t3 = (body("i"))
+t4 = (body("b"))
+t5 = (body("i"))
+t6 = (body("d"))
+t7 = (body("i"))
+
+
+
+#conditionals - sofia
+if wordz for letr1
+    print("corect!")
+    elif 
+
+
+
+
+
+oops - lindsey
+ 
+ rds= ["rizz", "eye", "skibidi"]
+for wrds in wrds:
+    print(f"Make your guess! {wrds}!")
+i=0
+while i<50: 
+    i=i+1 
+    if i%3==0 and i%5==0: 
+        print ("WRONG!" + "Try AGAIN!")
+    elif i%3==0: 
+        print("RIGHT!")
+    elif i%5==0: 
+        print("") 
+    else:
+        print (i)
