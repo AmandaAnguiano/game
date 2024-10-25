95594ad8deae81126bdb2f2013820bbad3305b76
AmandaAnguiano <amanda.anguiano@ucas-edu.net>
2024-10-25 00:44:38 +0000

    hella code
    Co-authored-by: s0fia090 <s0fia090@users.noreply.github.com>
    Co-authored-by: lindseyisSigma <lindseyisSigma@users.noreply.github.com>
    Co-authored-by: elliefonua <elliefonua@users.noreply.github.com>

--- build.a.game/game.py
+++ build.a.game/game.py
@@ -1,60 +1,20 @@
 
-#function of  - amanda
-def body(input):
-    question = (input(f"{input} was added....\n"))
-    
-    return question
-    
-    
-# Ask for letters - ellie
+# (variable) what each path causes to happen to the user - amanda
+print("Welcome to Gold Quest Island! There will be gold scattered all around the island, you only need to collect 5!\n") 
+path1 = ("You walk deeper into the jungle, you hit somthing on the ground and see its your first piece of gold. Just then a monkey comes and takes it from your hands and runs!")
+path2 = ("You continue walking the shore, after a few hours you find no gold.")
+# (function) ask the quests questions and where they want to go for the different locations on the game map- ellie
+jungle = path1
+shore = path2
+def location(side):
+    return f"Would you like to {side}"
 
+print(location("go into the jungle or keep walking the shore'?\n"))
+input()
+# (condition) conditoning wether the user takes path one or two and what happens when user chooses different answers - sofia
+if input() == shore:
+    print(path1)
+else:
+    print(path2)
+# (loop) how many lives the user has and wether they would like to restart or continue and controls how long the condition goes for until game is done - lindsey
 
-word1 = eye
-word2 = skibidi
-word3 = mog
-word4word5 = rizz
-
-l1 = (body("r"))
-l2 = (body("i"))
-l3 = (body("z"))
-l4 = (body("z"))
-
-e1 = (body("e"))
-e1 = (body("y"))
-e3 = (body("e"))
-
-t1 = (body("s"))
-t2 = (body("k"))
-t3 = (body("i"))
-t4 = (body("b"))
-t5 = (body("i"))
-t6 = (body("d"))
-t7 = (body("i"))
-
-
-
-#conditionals - sofia
-if wordz for letr1
-    print("corect!")
-    elif 
-
-
-
-
-
-oops - lindsey
- 
- rds= ["rizz", "eye", "skibidi"]
-for wrds in wrds:
-    print(f"Make your guess! {wrds}!")
-i=0
-while i<50: 
-    i=i+1 
-    if i%3==0 and i%5==0: 
-        print ("WRONG!" + "Try AGAIN!")
-    elif i%3==0: 
-        print("RIGHT!")
-    elif i%5==0: 
-        print("") 
-    else:
-        print (i)
